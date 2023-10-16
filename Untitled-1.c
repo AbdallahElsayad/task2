ece9e62ff7ff23e5c7d08538de209aa30922a93d
AbdallahElsayad <117679238+AbdallahElsayad@users.noreply.github.com>
2023-10-16 17:21:11 +0300

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    int op;
+    char f_name[10];
+    char l_name[10];
+    char user[20];
+    char email[30];
+    char pass[12];
+    char username[] = "abdo";
+    char password[] = "12345";
+    printf("Welcome Sir,\n Please Choose The Number Of The Operation You Want \n");
+    printf("1- Registration \n");
+    printf("2- Login \n");
+    printf("The Number Of Operation : ");
+    scanf("%d", &op);
+    if (op == 1)
+    {
+        printf("Enter You First Name : ");
+        scanf("%s", &f_name);
+        printf("Enter You Last Name : ");
+        scanf("%s", &l_name);
+        printf("Enter You Display Name : ");
+        scanf("%s", &user);
+        printf("Enter You Email : ");
+        scanf("%s", &email);
+        printf("Enter You Password : ");
+        scanf("%s", &pass);
+        printf("Registration Successful \n");
+        printf("You First Name Is : %s \n", f_name);
+        printf("You Last Name Is : %s \n", l_name);
+        printf("You Username Is : %s \n", username);
+        printf("You Email Is : %s \n", email);
+        printf("You Password Is : %s \n", pass);
+    }
+    else if (op == 2)
+    {
+        do
+        {
+            printf("Enter Username : ");
+            scanf("%s", &user);
+            printf("Enter The Password : ");
+            scanf("%s", &pass);
+            if (!strcmp(user, username) && !strcmp(pass, password))
+            {
+                printf("Login Success! \n");
+                break;
+            }
+            else
+            {
+                printf("Login Failled, Wrong Username or Password ,Try Again \n");
+            }
+        } while (1);
+    }
+    else
+    {
+        printf("Wrong Operation");
+    }
+}
+
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    int op;
+    char first_name[15];
+    char last_name[15];
+    char user[10];
+    char email[25];
+    char pass[10];
+    printf("Welcome \n choose the number of the operation \n");
+    printf("1. registration \n");
+    printf("2. login \n");
+    printf("The number of operation : ");
+    scanf("%d", &op);
+    if (op == 1)
+    {
+        printf("Enter You First Name : ");
+        scanf("%s", &first_name);
+        printf("Enter You Last Name : ");
+        scanf("%s", &last_name);
+        printf("Enter You Display Name : ");
+        scanf("%s", &user);
+        printf("Enter You Email : ");
+        scanf("%s", &email);
+        printf("Enter You Password : ");
+        scanf("%s", &pass);
+        printf("Registration Successful \n");
+    }
+    else if (op == 2)
+    {
+        printf("enter username : ");
+        scanf("%s", &user);
+        printf("enter password : ");
+        scanf("%s", &pass);
+        printf("login successfull");
+    }
+}
